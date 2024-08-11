bb390fdc8edece67ab23a504cb585eefd363e7e6
tsangd15 <77072827+tsangd15@users.noreply.github.com>
2024-08-11 17:04:37 +0100

    Restructure Code Order
    This commit moves the functions to the top of the file to be consistent
    with other files.
    Additionally, functions docstring shave been added.

--- main.py
+++ main.py
@@ -4,34 +4,19 @@ import threading
 import time
 import datetime
 
-HEADER_SIZE = 32
-IP = "0.0.0.0"  # 0.0.0.0 means listen on every ip assigned to computer
-PORT = 63632
-SERVER_ADDR = (IP, PORT)
-ENCODING = "utf-8"
-
-# create socket object
-# socket.AF_INET specifies IPv4, socket.SOCK_STREAM specifies TCP protocol
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# allow program to take ownership of ip address and port
-server_socket.bind(SERVER_ADDR)
-# listen on bound port
-server_socket.listen(5)  # backlog 5 connections
-
-# connection established
-# fixed size prep header contains payload size
-# payload (actual data) sent afterwards
-
 
 def log(message):
+    """Print a message to the console with a timestamp."""
     print(f"[{datetime.datetime.now()}] >>> {message}")
 
 
 def log_connections():
+    """Print active connections log message."""
     log(f"ACTIVE CONNECTIONS: {threading.active_count() - initial_threads}")
 
 
 def handle_client(client_socket, client_addr):
+    """Function for individual thread to run to handle each connection."""
     with client_socket as conn:
         iterations = 0
         while True:
@@ -61,6 +46,25 @@ def handle_client(client_socket, client_addr):
     log(f"CLOSED CONNECTION: {client_addr}")
 
 
+HEADER_SIZE = 32
+IP = "0.0.0.0"  # 0.0.0.0 means listen on every ip assigned to computer
+PORT = 63632
+SERVER_ADDR = (IP, PORT)
+ENCODING = "utf-8"
+
+# create socket object
+# socket.AF_INET specifies IPv4, socket.SOCK_STREAM specifies TCP protocol
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# allow program to take ownership of ip address and port
+server_socket.bind(SERVER_ADDR)
+# listen on bound port
+server_socket.listen(5)  # backlog 5 connections
+
+# connection established
+# fixed size prep header contains payload size
+# payload (actual data) sent afterwards
+
+# store initial threads (no connection threads)
 initial_threads = threading.active_count()
 
 while True:
