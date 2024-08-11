c7eb4317ecc5a7e492c4eb07a43228083cdc0ccf
tsangd15 <77072827+tsangd15@users.noreply.github.com>
2024-08-11 17:04:36 +0100

    Use DB Handler Functions Instead of Class
    This commit replaces the Database class with a set of functions that
    out the same operations.
    
    I proposed that a connection is created for each operation and then
    closes after it has finished. This wouldn't be very suitable in a class
    as it would have no attributes.
    This functionality has now been implemented in the form of functions.

--- db_handler.py
+++ db_handler.py
@@ -2,46 +2,36 @@
 import sqlite3
 
 
-class Database():
-    def __init__(self, db_name) -> None:
-        self.conn = sqlite3.connect(f"{db_name}.db")
+def connect(db_name):
+    """Return a database Connection object."""
+    conn = sqlite3.connect(f"{db_name}.db")
+    return conn
 
-    # def add_table(self):
-        # """Add a table to the database."""
-        # try:
-        #     with self.conn:
-        #         self.conn.execute("""CREATE TABLE leaderboard
-        #                           (tag text,
-        #                           score integer)""")
-        # except sqlite3.ProgrammingError as err:  # catch table already exists
-        #     # in event of exception, transaction rolled back
-        #     print(f"ProgrammingError: {err}")
 
-        # connection object context manager only commits/rollbacks
-        # transactions so it needs to be closed manually
-        # self.conn.close()
+def get_top(quantity):
+    """Return the top x scores from the database (in descending order).
+    The data is returned as a list of tuples. E.g. [("ABC", 74)]"""
+    conn = connect("data")
+    cur = conn.cursor()
+    with conn:
+        # get the top x scores from leaderboard ordered by score in
+        # descending order
+        cur.execute("""SELECT * FROM leaderboard
+                    ORDER BY score DESC
+                    LIMIT ?""", (quantity,))
 
-    def get_top(self, quantity):
-        """Return the top x scores from the database (in descending order)."""
-        cur = self.conn.cursor()
-        with self.conn:
-            # get the top x scores from leaderboard ordered by score in
-            # descending order
-            self.conn.execute("""SELECT * FROM leaderboard
-                                ORDER BY score DESC
-                                LIMIT ?""", (quantity,))
+    results = cur.fetchall()
 
-            print(cur.fetchall())
+    conn.close()
+    return results
 
-        print(cur.fetchall())
-        return cur.fetchall()
 
-    def add_entry(self, tag, score):
-        """Add an entry into the leaderboard table."""
-        with self.conn:
-            self.conn.execute("""INSERT INTO leaderboard (tag, score)
-                              VALUES (?, ?)""", (tag, score))
+def add_entry(tag, score):
+    """Add an entry into the leaderboard table."""
+    conn = connect("data")
 
-    def close(self):
-        """Close the Connection object."""
-        self.conn.close()
+    with conn:
+        conn.execute("""INSERT INTO leaderboard (tag, score)
+                     VALUES (?, ?)""", (tag, score))
+
+    conn.close()
